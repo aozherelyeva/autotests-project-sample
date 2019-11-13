4d2f430f4bfc8d014e1bee9dc7e7dc2451d1d1c4
aozherelyeva <aozherelyeva@gmail.com>
2019-11-13 17:48:30 +0300

    Fix tests for a product page

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 
 product_base_link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -17,8 +18,9 @@ def test_guest_can_go_to_login_page_from_product_page(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-    login_page = page.go_to_login_page()
-    login_page.should_be_login_page()
+    page.go_to_login_page()
+    page = LoginPage(browser, browser.current_url)
+    page.should_be_login_page()
 
 
 @pytest.mark.parametrize('link', [product_base_link])
@@ -30,6 +32,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.name_added_should_be_correct()
 
 
+@pytest.mark.xfail
 @pytest.mark.parametrize('link', [product_base_link])
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
     page = ProductPage(browser, link)
@@ -45,6 +48,7 @@ def test_guest_cant_see_success_message(browser, link):
     page.no_success_message()
 
 
+@pytest.mark.xfail
 @pytest.mark.parametrize('link', [product_base_link])
 def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page = ProductPage(browser, link)
@@ -53,3 +57,11 @@ def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page.success_message_is_disappeared()
 
 
+@pytest.mark.parametrize('link', [product_base_link])
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page = BasketPage(browser, browser.current_url)
+    page.basket_should_be_empty()
+    page.should_be_message_empty_basket()
